b3cb3a577a3bc05621f1cfe9ed7db4e14f0a0415
absaoban07 <absaoban@gmail.com>
2023-11-30 13:23:01 +0100

    hot fix

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ document.querySelector('.plusbtn').onclick = function(add){
     if (count < 5){
         count += 1;
     }
+    else{
+       ` ${alert("Note: we only have 5 in store for now")}`
+    }
     document.querySelector(".countitem").innerHTML= count;
     updateItemPrice();
 }
